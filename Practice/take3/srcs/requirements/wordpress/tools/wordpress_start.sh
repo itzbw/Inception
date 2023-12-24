d0ddea903398c0ff57efce1a584339fcebb44acb
itzbw <thebw.42@pm.me>
2023-12-24 02:23:54 +0100

    take3

--- Practice/take3/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ Practice/take3/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+sleep 10
+
 if [ ! -f /var/www/html/wp-config.php ];
-then 
+then
 	cd /var/www/html
 	wp core download --allow-root
 	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
 	wp core install --url=$WP_DOMAIN --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_EMAIL --allow-root
-	wp user create $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=editor --allow-root
-fi
+	wp user create $WP_USER_NAME $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
+	wp theme install astra --activate
+	wp plugin update --all
 
+fi
+# Start the PHP-FPM process
+# PHP-FPM stands for PHP FastCGI Process Manager.
+# It is a PHP extension that allows PHP to operate as a FastCGI process manager for web servers like Nginx
 /usr/sbin/php-fpm7.4 -F
+
+
+# When a request is made to the web server,
+#it passes the request to PHP-FPM, which then executes the PHP code and returns the result to the web server.
+#This allows for efficient handling of PHP requests and can significantly improve website performance.
