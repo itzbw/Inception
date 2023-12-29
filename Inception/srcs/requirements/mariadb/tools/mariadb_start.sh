2af104acf29490a07ac89f8f640a52dda984e060
Bonnie WONG <bwong@made-f0Ar4s1.clusters.42paris.fr>
2023-12-29 15:14:18 +0100

    inception 99%

--- Inception/srcs/requirements/mariadb/tools/mariadb_start.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -4,9 +4,11 @@ service mariadb start
 
 if [ ! -d /var/lib/mysql/$DB_NAME ];
 then
-	mariadb -u root -e "CREATE DATABASE $DB_NAME;"
-	mariadb -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-	mariadb -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
-	mariadb -u root -e "FLUSH PRIVILEGES;"
+	#mariadb -u root -e "DROP USER IF EXISTS ''@'localhost'"
+	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 	mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD'";
+	mariadb -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+	mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' WITH GRANT OPTION;"
+	mariadb -u root -e "FLUSH PRIVILEGES;"
+	
 fi
